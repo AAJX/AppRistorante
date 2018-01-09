5ad598a2c30ab9199dc9b42f7abe677652a0f646
monica <monica@192.168.0.188>
2018-01-09 17:26:22 +0100

    bean ristorante completo

--- eclipse/RistoranteJ/src/bean/Ristorante.java
+++ eclipse/RistoranteJ/src/bean/Ristorante.java
@@ -1,5 +1,35 @@
 package bean;
 
 public class Ristorante {
-
+	
+	private int nome;
+	private String indirizzo;
+    private String telefono;
+    private int prenotazioneUtenteIdCliente;
+    
+    public int getNome() {
+		return nome;
+	}
+	public void setNome(int nome) {
+		this.nome = nome;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public int getPrenotazioneUtenteIdCliente() {
+		return prenotazioneUtenteIdCliente;
+	}
+	public void setPrenotazioneUtenteIdCliente(int prenotazioneUtenteIdCliente) {
+		this.prenotazioneUtenteIdCliente = prenotazioneUtenteIdCliente;
+	}
+	
 }
