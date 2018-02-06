6e9bc239462553f5bc657ac270fa123dac2d33eb
monica <monica@192.168.0.63>
2018-02-06 16:54:08 +0100

    elimina

--- allyoucanapp/src/allyoucanapp/model2/GestionePagamenti.java
+++
@@ -1,8 +0,0 @@
-package allyoucanapp.model2;
-
-public enum GestionePagamenti {
-	PAGA_SUL_POSTO,
-	PAYPAL,
-	CARTA_DI_CREDITO,
-	POSTEPAY
-}
--- allyoucanapp/src/allyoucanapp/model2/MetodoDiPagamento.java
+++
@@ -1,9 +0,0 @@
-package allyoucanapp.model2;
-
-public enum MetodoDiPagamento {
-	PAGA_SUL_POSTO,
-	PAYPAL,
-	CARTA_DI_CREDITO,
-	POSTEPAY
-}
-
