315fc68803894dfd629a1f7edfad6f62801b8cf4
Andrea Colleoni <andrea@colleoni.info>
2018-02-14 15:24:41 +0100

    new app

---
+++ AppRistorantiWebTest/src/servlet/NuovaPrenotazioneServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import business.GestorePrenotazioni;
+import model.Prenotazione;
+import model.Ristorante;
+import model.Utente;
+
+/**
+ * Servlet implementation class NuovaPrenotazioneServlet
+ */
+@WebServlet("/nuovaPrenotazione")
+public class NuovaPrenotazioneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NuovaPrenotazioneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ObjectMapper om = new ObjectMapper();
+		
+		Utente utente = om.readValue("Utente", Utente.class);
+		Ristorante r = om.readValue("Ristorante", Ristorante.class);
+		Prenotazione prenotazione = om.readValue("Prenotazione", Prenotazione.class);
+		
+		GestorePrenotazioni gp = new GestorePrenotazioni();
+		
+		Boolean andataBuonFine = gp.nuovaPrenotazione(utente, prenotazione, r);
+		
+		response.getWriter().append(andataBuonFine.toString());
+	}
+
+}
